aaf87cef669d74a82c909f517c3dcf72ba8cf7a1
Johana Elizalde <njelizalde@uce.edu.ec>
2026-03-24 15:23:58 -0500

    holaaaaaaaaaaaaaa

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola, Mundoooooooooooo!");
-        System.out.println("holaaaa");
+        System.out.println("hola");
     }
 }
